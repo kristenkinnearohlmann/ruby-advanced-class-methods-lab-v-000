7182b93a64429c9e790f1e4c20950729a0cb6e29
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-06-22 22:21:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,32 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    #if self.all.any? { |song| song.name == name}
+    #    binding.pry
+    #end
+    binding.pry
+    self.all.select { |song| song.name == name}
   end
 
 end

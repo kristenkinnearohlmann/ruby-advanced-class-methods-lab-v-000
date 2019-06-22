b50aa51c53a9a71c13b60ac2927f16cb5c89d67b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-06-22 22:55:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,8 +53,8 @@ class Song
 
     song = self.create
     binding.pry
-    song.name = song_data[0]
-    song.artist_name = song_data[1]
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
   end
 
 end
